45b53c7b36c281cc4ab49e3cc348a96039d2d72b
Adrian <adrianamcovei17@gmail.com>
2022-11-21 15:42:05 +0200

    Add unit test code

--- app.py
+++ app.py
@@ -29,26 +29,33 @@ def menu():
             ask_create_post()
         selection = input(MENU_PROMPT)
 
+
 def print_blogs():
     # Print available blogs
     for key, blog in blogs.items():  # list of tuples
         print('-{}'.format(blog))
 
+
 def ask_create_blogs():
     title = input("Enter your blog title:\n")
     author = input("Enter your name:\n")
     blogs[title] = Blog(title, author)
+
+
 def ask_read_blog():
     title = input("Enter your blog title you want to read:\n")
 
     print_posts(blogs[title])
 
+
 def print_posts(blog):
     for post in blog.posts:
         print_post(post)
 
+
 def print_post(post):
     print(POST_TEMPLATE.format(post.title, post.content))
+
 
 def ask_create_post():
     blog_name = input("Enter the blog title you want to write a post in:\n")
